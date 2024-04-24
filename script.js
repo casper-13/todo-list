4524e631e0d799c34ea04ffeddf7460c476bb2ef
casper-13 <harish013901@gmail.com>
2024-04-24 23:26:16 +0530

    second

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var inputbox = document.getElementById("inputbox")
 function additem(){
 
     var listitem = document.createElement("li")
-    listitem.innerHTML = inputbox.value +"<button onclick='deleteitem(event)' id='delete'>Delete</button>"
+    listitem.innerHTML = inputbox.value +"<button onclick='deleteitem(event)' id='delete' class='delete'>Delete</button><hr class='line'>"
     ul.append(listitem)
 }
 
