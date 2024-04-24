05fe771d3c721cc58c58006932b0b0617cd2188a
casper-13 <harish013901@gmail.com>
2024-04-24 17:18:01 +0530

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+var ul = document.getElementById("list-container")
+var del = document.getElementById("delete")
+var inputbox = document.getElementById("inputbox")
+
+function additem(){
+
+    var listitem = document.createElement("li")
+    listitem.innerHTML = inputbox.value +"<button onclick='deleteitem(event)' id='delete'>Delete</button>"
+    ul.append(listitem)
+}
+
+function deleteitem(event){
+    event.target.parentElement.remove()
+}
